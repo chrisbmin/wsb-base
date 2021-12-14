9633c22ee0e87c89383e4d26aa247d43c9b58119
Chris <chrisrbmn@users.noreply.github.com>
2021-12-14 13:49:27 -0600

    adding some testing stuff

--- usb/runme.bat
+++ usb/runme.bat
@@ -6,5 +6,6 @@ REM Download the most up to date version of the script from the repository. down
 powershell.exe -ExecutionPolicy Bypass -noprofile -command "&{start-process powershell -ArgumentList '-noprofile -file \"%~dp0download.ps1\"' -verb RunAs}"
 REM The download.ps1 auto extracts the contents to c:\build. Wait 10 seconds for this to complete before executing the PCSetup.ps1 script
 timeout 10
+ECHO "Downloaded Successfully!"
 REM Execute the final build script
 REM powershell.exe -ExecutionPolicy Bypass -noprofile -command "&{start-process powershell -ArgumentList '-noprofile -file \"c:\build\PC-Build-Script-master\PCSetup.ps1\"' -verb RunAs}"
