53f830fa4ec26583c036c39facb0cb638a7ff258
Chris <chrisrbmn@users.noreply.github.com>
2025-02-26 14:23:05 -0600

    Update InstallBaseTools.ps1
    accepting all the source and package agreements.

--- scripts/InstallBaseTools.ps1
+++ scripts/InstallBaseTools.ps1
@@ -6,10 +6,10 @@
 # Browsers  ------------------------------------ #
 # ---------------------------------------------- #
 # choco install -y googlechrome
-winget install -e --accept-package-agreements --id Google.Chrome 
+winget install -e --accept-source-agreements --accept-package-agreements --id Google.Chrome 
 
 # choco install -y firefox
-winget install -e --accept-package-agreements --id Mozilla.Firefox
+winget install -e --accept-source-agreements --accept-package-agreements --id Mozilla.Firefox
 
 
 # ---------------------------------------------- #
@@ -17,28 +17,28 @@ winget install -e --accept-package-agreements --id Mozilla.Firefox
 # ---------------------------------------------- #
 
 # choco install -y 7zip
-winget install -e --id 7zip.7zip 
+winget install -e --accept-source-agreements --accept-package-agreements --id 7zip.7zip 
 
 # choco install -y ShareX
-winget install -e --id ShareX.ShareX 
+winget install -e --accept-source-agreements --accept-package-agreements --id ShareX.ShareX 
  
-winget install -e --id WinMerge.WinMerge
-winget install -e --id dotPDN.PaintDotNet
-winget install -e --id Notepad++.Notepad++
-winget install -e -h --id JackieLiu.NotepadsApp
-winget install -e -h --id AntibodySoftware.WizTree
-winget install -e -h --id WinSCP.WinSCP
+winget install -e --accept-source-agreements --accept-package-agreements --id WinMerge.WinMerge
+winget install -e --accept-source-agreements --accept-package-agreements --id dotPDN.PaintDotNet
+winget install -e --accept-source-agreements --accept-package-agreements --id Notepad++.Notepad++
+winget install -e --accept-source-agreements --accept-package-agreements -h --id JackieLiu.NotepadsApp
+winget install -e --accept-source-agreements --accept-package-agreements -h --id AntibodySoftware.WizTree
+winget install -e --accept-source-agreements --accept-package-agreements -h --id WinSCP.WinSCP
 
 # Install FileZilla using Chocolatey because it's not available via winget
 choco install -y filezilla
 
-winget install -e --accept-package-agreements --id 9nt1r1c2hh7j
+winget install -e --accept-source-agreements --accept-package-agreements --accept-package-agreements --id 9nt1r1c2hh7j
 
 # ---------------------------------------------- #
 # Windows Terminal ----------------------------- #
 # ---------------------------------------------- #
 # Windows Terminal (stable + preview) install with Cascadia Code PL font
-winget install -e -h --id Microsoft.WindowsTerminal -s msstore
+winget install -e --accept-source-agreements --accept-package-agreements -h --id Microsoft.WindowsTerminal -s msstore
 # winget install -e -h --id Microsoft.WindowsTerminalPreview -s msstore
 
 # Windows terminal configuration
@@ -54,7 +54,7 @@ winget install -e -h --id Microsoft.WindowsTerminal -s msstore
 # ---------------------------------------------- #
 # PowerShell  ---------------------------------- #
 # ---------------------------------------------- #
-winget install -e -h --id Microsoft.PowerShell
+winget install -e --accept-source-agreements --accept-package-agreements -h --id Microsoft.PowerShell
 # Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 #Remove-Item -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Force
 #New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Target "$env:USERPROFILE\wsb\config\powerShell\Microsoft.PowerShell_profile.ps1"
@@ -67,10 +67,10 @@ winget install -e -h --id Microsoft.PowerShell
 # iwr -useb get.scoop.sh | iex
 
 #winget install -e -h --id GitHub.cli
-winget install -e --id GitHub.GitHubDesktop
+winget install -e --accept-source-agreements --accept-package-agreements --id GitHub.GitHubDesktop
 
 # Chocolatey version is more up-to-date than winget version
 #choco install -y vscode
-winget install -e --id Microsoft.VisualStudioCode
+winget install -e --accept-source-agreements --accept-package-agreements --id Microsoft.VisualStudioCode
 
 
