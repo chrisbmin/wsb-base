4afedffddde2bdfb97b6d8a623569c9dac655125
Chris <chrisrbmn@users.noreply.github.com>
2025-02-27 09:35:33 -0600

    Update InstallBaseTools.ps1

--- scripts/InstallBaseTools.ps1
+++ scripts/InstallBaseTools.ps1
@@ -39,34 +39,18 @@ winget install -e --accept-source-agreements --accept-package-agreements --accep
 # ---------------------------------------------- #
 # Windows Terminal ----------------------------- #
 # ---------------------------------------------- #
-# Windows Terminal (stable + preview) install with Cascadia Code PL font
+# Windows Terminal (stable)
 winget install -e --accept-source-agreements --accept-package-agreements -h --id Microsoft.WindowsTerminal -s msstore
-# winget install -e -h --id Microsoft.WindowsTerminalPreview -s msstore
-
-# Windows terminal configuration
-#Remove-Item -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force
-#New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Target "$env:USERPROFILE\wsb\config\windowsTerminal\settings.json"
-#cp "$env:USERPROFILE\wsb\config\windowsTerminal\icons\*" "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\RoamingState\"
-# Windows terminal preview configuration
-#Remove-Item -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" -Force
-#New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" -Target "$env:USERPROFILE\wsb\config\windowsTerminal\settings.json"
-#cp "$env:USERPROFILE\wsb\config\windowsTerminal\icons\*" "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\RoamingState\"
-
 
 # ---------------------------------------------- #
 # PowerShell  ---------------------------------- #
 # ---------------------------------------------- #
 winget install -e --accept-source-agreements --accept-package-agreements -h --id Microsoft.PowerShell
-# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-#Remove-Item -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Force
-#New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Target "$env:USERPROFILE\wsb\config\powerShell\Microsoft.PowerShell_profile.ps1"
 
 
 # ---------------------------------------------- #
 # Dev tools  ----------------------------------- #
 # ---------------------------------------------- #
-# https://scoop.sh/
-# iwr -useb get.scoop.sh | iex
 
 #winget install -e -h --id GitHub.cli
 winget install -e --accept-source-agreements --accept-package-agreements --id GitHub.GitHubDesktop
@@ -74,5 +58,3 @@ winget install -e --accept-source-agreements --accept-package-agreements --id Gi
 # Chocolatey version is more up-to-date than winget version
 #choco install -y vscode
 winget install -e --accept-source-agreements --accept-package-agreements --id Microsoft.VisualStudioCode
-
-
